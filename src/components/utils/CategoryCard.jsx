c847283976730add6a90580d3dc9b385a26aa9b6
arup2071 <arup124820@gmail.com>
2025-05-18 11:20:31 +0600

    .....

--- src/components/utils/CategoryCard.jsx
+++ src/components/utils/CategoryCard.jsx
@@ -3,9 +3,8 @@ import { Link } from "react-router-dom";
 
 const CategoryCard = ({ title, url = "" }) => {
   return (
-    
     <div className="bg-white pt-4 sm:pt-6 md:pt-8 pb-4 sm:pb-5 md:pb-6 text-center w-full sm:max-w-[180px] md:max-w-[200px] rounded-md border border-gray-300 hover:border-branding-success-dark hover:border-3 hover:shadow-[0px_0px_12px_0px_rgba(32,181,38,0.32)] transition-all">
-      <Link>
+      <Link to={`/category/${url}`}>
         <img
           src="/images/CategoryIcon1.png"
           alt=""
